55d649911c84dfc79c759dcfd142289dc1ef9997
maxrok98 <maxrok98@gmail.com>
2020-08-21 22:31:02 +0300

    Added creating(CreatePost) and deleting(UserPage) posts

--- Forum.Shared/Contracts/Responses/UserResponse.cs
+++ Forum.Shared/Contracts/Responses/UserResponse.cs
@@ -14,7 +14,7 @@ namespace Forum.Contracts.Responses
         public string ImageId { get; set; }
         public byte[] Image { get; set; }
 
-        public virtual ICollection<PostForUserResponse> myPosts { get; set; }
+        public virtual List<PostForUserResponse> myPosts { get; set; }
         public virtual ICollection<ThreadResponse> myThread { get; set; }
     }
 }
