8ad4f1825c51551ef74f750a131988ab274486b3
maxrok98 <max.rok.98@gmail.com>
2021-06-01 14:46:11 +0300

    Add text editor for post content

---
+++ Forum.Client/wwwroot/js/summernote-functions.js
@@ -0,0 +1,9 @@
+﻿function createNote() {
+    $('#summernote').summernote();
+}
+function getCode() {
+    return $('#summernote').summernote('code');
+}
+function destroyNote(){
+    $('#summernote').summernote('destroy');
+}
--- Forum.Shared/Contracts/Requests/PostRequest.cs
+++ Forum.Shared/Contracts/Requests/PostRequest.cs
@@ -15,7 +15,7 @@ namespace Forum.Contracts.Requests
 
         [Required]
         public string Name { get; set; }
-        [Required]
+
         public string Content { get; set; }
     
         public byte[] Image { get; set; }
